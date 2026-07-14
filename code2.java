a16d7bb7f976a72c8c1f8046ca11373c4449f259
Aravinth MG <aravinthmg2006@gmail.com>
2026-07-14 16:57:29 +0530

    code2

---
+++ code2.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String[] args) {
+        int n=30;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
